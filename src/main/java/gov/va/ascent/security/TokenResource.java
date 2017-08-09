ef6d3588e9890de306651c3599e0aa835009c918
vgaddavcg <vgadda@meetveracity.com>
2017-08-09 12:49:27 -0400

    updated for autoconfigure/starter

---
+++ src/main/java/gov/va/ascent/security/TokenResource.java
@@ -0,0 +1,27 @@
+package gov.va.ascent.security;
+
+import gov.va.ascent.security.jwt.JwtAuthenticationProperties;
+import gov.va.ascent.security.util.GenerateToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import gov.va.ascent.security.jwt.PersonTraits;
+
+@RestController
+public class TokenResource {
+
+	private final static Logger LOG = LoggerFactory.getLogger(TokenResource.class);
+
+	@Autowired
+	private JwtAuthenticationProperties jwtAuthenticationProperties;
+
+	@RequestMapping(value = "/token", method = RequestMethod.POST, consumes = {"application/json"})
+	public String getToken(@RequestBody PersonTraits person){
+		return GenerateToken.generateJwt(person, jwtAuthenticationProperties.getSecret());
+	}
+}
