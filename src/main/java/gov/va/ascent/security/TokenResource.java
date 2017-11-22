43e79b1fdb0be9a94a39b0e1b9c83a0174362daa
Raju <raju.thota@icfi.com>
2017-11-22 11:16:25 -0500

    ST-1058 - Fixed fortify vioaltion

--- src/main/java/gov/va/ascent/security/TokenResource.java
+++ src/main/java/gov/va/ascent/security/TokenResource.java
@@ -4,6 +4,9 @@ import gov.va.ascent.framework.security.PersonTraits;
 import gov.va.ascent.security.jwt.JwtAuthenticationProperties;
 import gov.va.ascent.security.util.GenerateToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.DataBinder;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,5 +21,18 @@ public class TokenResource {
 	@RequestMapping(value = "/token", method = RequestMethod.POST, consumes = {"application/json"})
 	public String getToken(@RequestBody PersonTraits person){
 		return GenerateToken.generateJwt(person, jwtAuthenticationProperties.getSecret());
+	}
+	
+	/**
+	 * Registers fields that should be allowed for data binding.
+	 * 
+	 * @param binder
+	 *            Spring-provided data binding context object.
+	 */
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.setAllowedFields(new String[] { "birthDate", "firstName", "lastName", "middleName", "prefix", "suffix",
+				"gender", "assuranceLevel", "email", "dodedipnid", "pnidType", "pnid", "pid", "icn", "fileNumber",
+				"tokenId", "correlationIds" });
 	}
 }
