32cc84b6ba13fe128ea3bd3d3f6fdba87cfb79a9
varun-akula <vakula@meetveracity.com>
2018-10-09 10:48:04 -0400

    DS-110 Fix bug with jwt token generation

--- src/main/java/gov/va/ascent/security/jwt/JwtParser.java
+++ src/main/java/gov/va/ascent/security/jwt/JwtParser.java
@@ -18,58 +18,55 @@ import io.jsonwebtoken.SignatureAlgorithm;
 
 public class JwtParser {
 
-    private JwtAuthenticationProperties jwtAuthenticationProperties;
+	private JwtAuthenticationProperties jwtAuthenticationProperties;
 
-    public JwtParser(JwtAuthenticationProperties properties){
-        this.jwtAuthenticationProperties = properties;
-    }
+	public JwtParser(final JwtAuthenticationProperties properties) {
+		this.jwtAuthenticationProperties = properties;
+	}
 
-    public PersonTraits parseJwt(String token){
-    	Claims claims = null;
-        
-        //The JWT signature algorithm we will be using to sign the token
-        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
-        
-        //We will sign our JWT with our ApiKey secret
-        Key signingKey = new SecretKeySpec(
-        					jwtAuthenticationProperties.getSecret().getBytes(StandardCharsets.UTF_8), 
-        						signatureAlgorithm.getJcaName());
-        
-        claims = Jwts.parser()
-        		.setSigningKey(signingKey)
-                .requireIssuer(jwtAuthenticationProperties.getIssuer())
-                .parseClaimsJws(token).getBody();
-        return getPersonFrom(claims);
+	public PersonTraits parseJwt(final String token) {
+		Claims claims = null;
 
-    }
+		// The JWT signature algorithm we will be using to sign the token
+		SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
-    private PersonTraits getPersonFrom(final Claims claims){
-        PersonTraits personTraits = new PersonTraits();
-        personTraits.setTokenId(claims.getId());
-        personTraits.setFirstName(claims.get("firstName", String.class));
-        personTraits.setLastName(claims.get("lastName", String.class));
-        personTraits.setPrefix(claims.get("prefix", String.class));
-        personTraits.setMiddleName(claims.get("middleName", String.class));
-        personTraits.setSuffix(claims.get("suffix", String.class));
-        personTraits.setBirthDate(claims.get("birthDate", String.class));
-        personTraits.setGender(claims.get("gender", String.class));
-        personTraits.setAssuranceLevel(claims.get("assuranceLevel", Integer.class));
-        personTraits.setEmail(claims.get("email", String.class));
-        
-        CorrelationIdsParser instance = new CorrelationIdsParser();
-        @SuppressWarnings("unchecked")
+		// We will sign our JWT with our ApiKey secret
+		Key signingKey = new SecretKeySpec(jwtAuthenticationProperties.getSecret().getBytes(StandardCharsets.UTF_8),
+				signatureAlgorithm.getJcaName());
+
+		claims = Jwts.parser().setSigningKey(signingKey).requireIssuer(jwtAuthenticationProperties.getIssuer()).parseClaimsJws(token)
+				.getBody();
+		return getPersonFrom(claims);
+
+	}
+
+	private PersonTraits getPersonFrom(final Claims claims) {
+		PersonTraits personTraits = new PersonTraits();
+		personTraits.setTokenId(claims.getId());
+		personTraits.setFirstName(claims.get("firstName", String.class));
+		personTraits.setLastName(claims.get("lastName", String.class));
+		personTraits.setPrefix(claims.get("prefix", String.class));
+		personTraits.setMiddleName(claims.get("middleName", String.class));
+		personTraits.setSuffix(claims.get("suffix", String.class));
+		personTraits.setBirthDate(claims.get("birthDate", String.class));
+		personTraits.setGender(claims.get("gender", String.class));
+		personTraits.setAssuranceLevel(claims.get("assuranceLevel", Integer.class));
+		personTraits.setEmail(claims.get("email", String.class));
+		personTraits.setDodedipnid(claims.get("dodedipnid", String.class));
+		personTraits.setPnidType(claims.get("pnidType", String.class));
+		personTraits.setPnid(claims.get("pnid", String.class));
+		personTraits.setPid(claims.get("pid", String.class));
+		personTraits.setIcn(claims.get("icn", String.class));
+		personTraits.setFileNumber(claims.get("fileNumber", String.class));
+
+		CorrelationIdsParser instance = new CorrelationIdsParser();
+		@SuppressWarnings("unchecked")
 		List<String> list = (List<String>) claims.get("correlationIds");
-        Map<String, String> result = instance.parseCorrelationIds(list);
-        
-        personTraits.setCorrelationIds(list);
-        personTraits.setDodedipnid(result.get("dodedipnid"));
-        personTraits.setPnidType(result.get("pnidType"));
-        personTraits.setPnid(result.get("pnid"));
-        personTraits.setPid(result.get("pid"));
-        personTraits.setIcn(result.get("icn"));
-        personTraits.setFileNumber(result.get("fileNumber"));
-        
-        return personTraits;
-    }
+		instance.parseCorrelationIds(list);
+
+		personTraits.setCorrelationIds(list);
+
+		return personTraits;
+	}
 
 }
