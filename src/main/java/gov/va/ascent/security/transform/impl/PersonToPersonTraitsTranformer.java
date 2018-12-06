c11e555eb944ef9c727aa514cadb841236132b79
varun-akula <vakula@meetveracity.com>
2018-12-06 11:29:57 -0500

    RDS-105 Use latest version of ascent-security, 0.0.12-SNAPSHOT

--- src/main/java/gov/va/ascent/security/transform/impl/PersonToPersonTraitsTranformer.java
+++ src/main/java/gov/va/ascent/security/transform/impl/PersonToPersonTraitsTranformer.java
@@ -2,12 +2,17 @@ package gov.va.ascent.security.transform.impl;
 
 import java.util.List;
 
+import gov.va.ascent.framework.exception.AscentRuntimeException;
+import gov.va.ascent.framework.log.AscentLogger;
+import gov.va.ascent.framework.log.AscentLoggerFactory;
 import gov.va.ascent.framework.security.PersonTraits;
 import gov.va.ascent.security.jwt.correlation.CorrelationIdsParser;
 import gov.va.ascent.security.model.Person;
 import gov.va.ascent.security.transform.AbstractProviderSecurityTransformer;
 
 public class PersonToPersonTraitsTranformer extends AbstractProviderSecurityTransformer<Person, PersonTraits> {
+
+	static final AscentLogger LOGGER = AscentLoggerFactory.getLogger(PersonToPersonTraitsTranformer.class);
 
 	@Override
 	public PersonTraits transformToService(final Person toTransform) {
@@ -31,7 +36,8 @@ public class PersonToPersonTraitsTranformer extends AbstractProviderSecurityTran
 
 		} catch (Exception e) { // NOSONAR intentionally wide, errors are already logged
 			// if there is any detected issue with the correlation ids
-			personTraits = null;
+			LOGGER.error("Unable to parse correlation Ids", e);
+			throw new AscentRuntimeException("Unable to parse correlation Ids", e);
 		}
 
 		return personTraits;
